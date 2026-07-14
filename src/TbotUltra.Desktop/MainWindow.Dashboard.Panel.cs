78a9972b5f24895f08cc53a71e6c0cbccd3c9af7
fetmamm <jesperr.svensson@gmail.com>
2026-07-15 00:22:39 +0200

    refactor(ui): extract dashboard panel

---
+++ src/TbotUltra.Desktop/MainWindow.Dashboard.Panel.cs
@@ -0,0 +1,39 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace TbotUltra.Desktop;
+
+public partial class MainWindow
+{
+    private Button ResetProgramButton => DashboardPanelControl.ResetButton;
+    private TextBlock VillagesInfoTextBlock => DashboardPanelControl.VillagesInfo;
+    private TextBlock LastScanInfoTextBlock => DashboardPanelControl.LastScanInfo;
+    private Ellipse AutomationLoopRunStateDot => DashboardPanelControl.AutomationRunStateDot;
+    private TextBlock AutomationLoopRunStateTextBlock => DashboardPanelControl.AutomationRunStateText;
+    private ListBox AutomationLoopListBox => DashboardPanelControl.AutomationLoopList;
+    private CheckBox AutoCollectTasksCheckBox => DashboardPanelControl.AutoCollectTasks;
+    private CheckBox AutoCollectDailyQuestsCheckBox => DashboardPanelControl.AutoCollectDailyQuests;
+    private CheckBox HeroResourceTransferCheckBox => DashboardPanelControl.HeroResourceTransfer;
+    private CheckBox ConstructFasterCheckBox => DashboardPanelControl.ConstructFaster;
+    private CheckBox ProductionBonusVideoCheckBox => DashboardPanelControl.ProductionBonusVideo;
+    private ItemsControl DashboardVillageList => DashboardPanelControl.VillageList;
+
+    internal void OnResetProgramClicked(object sender, RoutedEventArgs e) => ResetProgramButton_Click(sender, e);
+    internal void OnDashboardClearTimersClicked(object sender, RoutedEventArgs e) => DashboardClearTimersButton_Click(sender, e);
+    internal void OnAutomationLoopToggleClicked(object sender, RoutedEventArgs e) => AutomationLoopToggleButton_Click(sender, e);
+    internal void OnAutomationLoopPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) => AutomationLoopListBox_PreviewMouseLeftButtonDown(sender, e);
+    internal void OnAutomationLoopPreviewMouseMove(object sender, MouseEventArgs e) => AutomationLoopListBox_PreviewMouseMove(sender, e);
+    internal void OnAutomationLoopDragOver(object sender, DragEventArgs e) => AutomationLoopListBox_DragOver(sender, e);
+    internal void OnAutomationLoopDrop(object sender, DragEventArgs e) => AutomationLoopListBox_Drop(sender, e);
+    internal void OnAutoCollectTasksSettingChanged(object sender, RoutedEventArgs e) => AutoCollectTasksSetting_Changed(sender, e);
+    internal void OnAutoCollectDailyQuestsSettingChanged(object sender, RoutedEventArgs e) => AutoCollectDailyQuestsSetting_Changed(sender, e);
+    internal void OnHeroResourceTransferSettingChanged(object sender, RoutedEventArgs e) => HeroResourceTransferSetting_Changed(sender, e);
+    internal void OnHeroInventorySettingsClicked(object sender, RoutedEventArgs e) => HeroInventorySettingsButton_Click(sender, e);
+    internal void OnGoldSpendingSettingsClicked(object sender, RoutedEventArgs e) => GoldSpendingSettingsButton_Click(sender, e);
+    internal void OnConstructFasterSettingChanged(object sender, RoutedEventArgs e) => ConstructFasterSetting_Changed(sender, e);
+    internal void OnConstructFasterSettingsClicked(object sender, RoutedEventArgs e) => ConstructFasterSettingsButton_Click(sender, e);
+    internal void OnProductionBonusVideoSettingChanged(object sender, RoutedEventArgs e) => ProductionBonusVideoSetting_Changed(sender, e);
+    internal void OnProductionBonusSettingsClicked(object sender, RoutedEventArgs e) => ProductionBonusSettingsButton_Click(sender, e);
+}
---
+++ src/TbotUltra.Desktop/Views/DashboardPanel.xaml.cs
@@ -0,0 +1,45 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace TbotUltra.Desktop.Views;
+
+public partial class DashboardPanel : UserControl
+{
+    private MainWindow? _host;
+
+    public DashboardPanel() => InitializeComponent();
+
+    private MainWindow? Host => _host ??= Window.GetWindow(this) as MainWindow;
+
+    internal Button ResetButton => ResetProgramButton;
+    internal TextBlock VillagesInfo => VillagesInfoTextBlock;
+    internal TextBlock LastScanInfo => LastScanInfoTextBlock;
+    internal Ellipse AutomationRunStateDot => AutomationLoopRunStateDot;
+    internal TextBlock AutomationRunStateText => AutomationLoopRunStateTextBlock;
+    internal ListBox AutomationLoopList => AutomationLoopListBox;
+    internal CheckBox AutoCollectTasks => AutoCollectTasksCheckBox;
+    internal CheckBox AutoCollectDailyQuests => AutoCollectDailyQuestsCheckBox;
+    internal CheckBox HeroResourceTransfer => HeroResourceTransferCheckBox;
+    internal CheckBox ConstructFaster => ConstructFasterCheckBox;
+    internal CheckBox ProductionBonusVideo => ProductionBonusVideoCheckBox;
+    internal ItemsControl VillageList => DashboardVillageList;
+
+    private void ResetProgramButton_Click(object sender, RoutedEventArgs e) => Host?.OnResetProgramClicked(sender, e);
+    private void DashboardClearTimersButton_Click(object sender, RoutedEventArgs e) => Host?.OnDashboardClearTimersClicked(sender, e);
+    private void AutomationLoopToggleButton_Click(object sender, RoutedEventArgs e) => Host?.OnAutomationLoopToggleClicked(sender, e);
+    private void AutomationLoopListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) => Host?.OnAutomationLoopPreviewMouseLeftButtonDown(sender, e);
+    private void AutomationLoopListBox_PreviewMouseMove(object sender, MouseEventArgs e) => Host?.OnAutomationLoopPreviewMouseMove(sender, e);
+    private void AutomationLoopListBox_DragOver(object sender, DragEventArgs e) => Host?.OnAutomationLoopDragOver(sender, e);
+    private void AutomationLoopListBox_Drop(object sender, DragEventArgs e) => Host?.OnAutomationLoopDrop(sender, e);
+    private void AutoCollectTasksSetting_Changed(object sender, RoutedEventArgs e) => Host?.OnAutoCollectTasksSettingChanged(sender, e);
+    private void AutoCollectDailyQuestsSetting_Changed(object sender, RoutedEventArgs e) => Host?.OnAutoCollectDailyQuestsSettingChanged(sender, e);
+    private void HeroResourceTransferSetting_Changed(object sender, RoutedEventArgs e) => Host?.OnHeroResourceTransferSettingChanged(sender, e);
+    private void HeroInventorySettingsButton_Click(object sender, RoutedEventArgs e) => Host?.OnHeroInventorySettingsClicked(sender, e);
+    private void GoldSpendingSettingsButton_Click(object sender, RoutedEventArgs e) => Host?.OnGoldSpendingSettingsClicked(sender, e);
+    private void ConstructFasterSetting_Changed(object sender, RoutedEventArgs e) => Host?.OnConstructFasterSettingChanged(sender, e);
+    private void ConstructFasterSettingsButton_Click(object sender, RoutedEventArgs e) => Host?.OnConstructFasterSettingsClicked(sender, e);
+    private void ProductionBonusVideoSetting_Changed(object sender, RoutedEventArgs e) => Host?.OnProductionBonusVideoSettingChanged(sender, e);
+    private void ProductionBonusSettingsButton_Click(object sender, RoutedEventArgs e) => Host?.OnProductionBonusSettingsClicked(sender, e);
+}
